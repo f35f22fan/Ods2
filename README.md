0c595b22172439ce3fa670fd082a2dd108917398
Brian Warner <f35f22fan@gmail.com>
2020-10-19 05:00:57 +0300

    Build files updated

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-Ods2 is written from scratch to address QOds's core shortcommings:
- - [x] Allow for editing (updating) .ods files
- - [x] Allow for a full test suite
- - [x] A much more sound core structure.
+Ods2 is QOds rewritten from scratch to allow:
+ - [x] Updating of .ods files
+ - [x] Having a test suite
+ - [x] Implementing advanced features
 
 Built exactly as QOds, see subfolder sample/* for examples on how to use.
 
-For questions, feature requests, etc: f35f22fan AT gmail.com
-
-The file sample/Invoice.ods was created with Ods2, the source code
+The file sample/Invoice.ods was generated with Ods2, the source code
 is in sample/Invoice.cpp
-
-Licensed under the GPLv3, see LICENSE for details.
 
 #### TBD
  - [ ] Test building process on windows
+
+Licensed under the GPLv3, see LICENSE for details.
+For questions, feature requests, etc: f35f22fan AT gmail.com
--- setup_windows_env.bat
+++ setup_windows_env.bat
@@ -2,35 +2,35 @@
 echo Setting up the Window$ Qt environment...
 
 
+rem ==> ENVIRONMENT: MinGW
+set PATH=C:\Qt\Qt5.12.9\5.12.9\mingw73_64\bin;%PATH%
 
-rem ==> MinGW
-set PATH=C:\Qt\Qt5.10.1\5.10.1\mingw53_32\bin;%PATH%
-set PATH=C:\Qt\Qt5.10.1\Tools\mingw530_32\bin;%PATH%
-
-rem Dir with make.exe:
-set PATH=C:\Program Files (x86)\GnuWin32\bin;%PATH%
+set PATH=C:\Qt\Qt5.12.9\Tools\mingw730_64\bin;%PATH%
+rem IMPORTANT: also rename mingw32-make.exe
+rem to make.exe from the folder above (...tools/mingw/bin)
+rem so that the system can find the make.exe tool.
 
 rem Dir with cmake.exe:
 set PATH=C:\Program Files\CMake\bin;%PATH%
-rem <== MinGW
+rem <== ENVIRONMENT: MinGW
 
 
 
-rem ==> Visual Studio 2013 32bit
+rem ==> ENVIRONMENT: Visual Studio 2013 32bit
 rem set PATH=C:\Qt\Qt5.3.2\5.3\msvc2013_opengl\bin;%PATH%
 
 rem Dir with cmake.exe:
 rem set PATH=C:\Program Files\CMake\bin;%PATH%
-rem <== Visual Studio 2013 32bit
+rem <== ENVIRONMENT: Visual Studio 2013 32bit
 
 
 
-rem ==> Visual Studio 2015 64bit
+rem ==> ENVIRONMENT: Visual Studio 2015 64bit
 rem set PATH=C:\Qt\Qt5.7.1\5.7\msvc2015_64\bin;%PATH%
 
 rem Dir with cmake.exe:
 rem set PATH=C:\Program Files\CMake\bin;%PATH%
-rem <== Visual Studio 2013 64bit
+rem <== ENVIRONMENT: Visual Studio 2013 64bit
 
 
 
