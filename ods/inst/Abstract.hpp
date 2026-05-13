d1e3e881b8cfb3730845a46ef729e6a3533374db
Sebastian Schmidt <Sebastian.Schmidt@EasternGraphics.com>
2026-05-13 10:56:13 +0200

    [FIX] MSVC warning C4717 (endless recursion)

--- ods/inst/Abstract.hpp
+++ ods/inst/Abstract.hpp
@@ -37,11 +37,11 @@ enum class ClonePart: u8 {
 
 inline ClonePart operator | (ClonePart a, ClonePart b)
 {
-	return static_cast<ClonePart>(a | b);
+	return static_cast<ClonePart>(static_cast<u8>(a) | static_cast<u8>(b));
 }
 
 inline bool operator & (ClonePart a, ClonePart b) {
-	return a & b;
+	return (static_cast<u8>(a) & static_cast<u8>(b)) != 0;
 }
 
 inline void Add(const Prefix *prefix, NsHash &list)
